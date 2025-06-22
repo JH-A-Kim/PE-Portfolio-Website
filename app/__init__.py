a5eb5fa3a0828cbf1c69b5aaa48f72dbfb4c2ce4
jak28 <jak28@sfu.ca>
2025-06-21 19:42:21 -0700

    fixed img path for hobbies

--- app/__init__.py
+++ app/__init__.py
@@ -56,17 +56,17 @@ def more():
         {
             "hobby": "Video Games",
             "description": "Enjoy playing a variety of video games specifically League and TFT.",
-            "image": "./static/images/T1Art.jpeg"
+            "image": "./static/img/T1Art.jpeg"
         },
         {
             "hobby": "Organzing Hackathons",
             "description": "Passionate about organizing hackathons and tech events to foster innovation and collaboration.",
-            "image": "./static/images/StormHacks.jpg"
+            "image": "./static/img/StormHacks.jpg"
         },
         {
             "hobby": "Watching Esports",
             "description": "Avid fan of esports, particularly League of Legends (Huge fan of T1 and Faker).",
-            "image": "./static/images/EsportsWatching.PNG"
+            "image": "./static/img/EsportsWatching.PNG"
         }
     ]
 
